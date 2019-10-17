d5f4a42d3ab2bc99d3e4b27b0cdf3e6e3f86bfed
jesse <jesse@localhost.localdomain>
2019-10-17 21:00:41 +0000

    fix container names

--- docker_start_time_servers.sh
+++ docker_start_time_servers.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker run --restart unless-stopped --name api1 -d -p 81:80 -v $PWD/api1/:/app webdevops/php-nginx
-docker run --restart unless-stopped --name api2 -d -p 82:80 -v $PWD/api2/:/app webdevops/php-nginx
+docker container rm -f workshop-api1 workshop-api2
+docker run --restart unless-stopped --name workshop-api1 -d -p 81:80 -v $PWD/api1/:/app webdevops/php-nginx
+docker run --restart unless-stopped --name workshop-api2 -d -p 82:80 -v $PWD/api2/:/app webdevops/php-nginx
