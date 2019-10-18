f447c6d6629ab5295a5f67c73e7bee5f0b9a63b4
jesse <jesse@localhost.localdomain>
2019-10-18 20:27:08 +0000

    Fix script

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker-compose -f ergast-f1-api/docker-compose.yaml up -d
-sh docker_start_time_servers.sh
-
--- start_containers.sh
+++ start_containers.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+docker container rm -f workshop-f1-api workshop-db
+docker-compose -f ergast-f1-api/docker-compose.yaml up -d
+sh docker_start_time_servers.sh
+
 docker container rm -f workshop-api1 workshop-api2
 docker run --restart unless-stopped --name workshop-api1 -d -p 81:80 -v $PWD/api1/:/app webdevops/php-nginx
 docker run --restart unless-stopped --name workshop-api2 -d -p 82:80 -v $PWD/api2/:/app webdevops/php-nginx
